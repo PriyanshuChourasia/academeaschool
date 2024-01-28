2cd5dac0becb0f10af91bf10c6a6a9055b7555e9
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2024-01-28 11:52:21 +0530

    copy rifgt changes

--- src/components/copyright/CopyRight.jsx
+++ src/components/copyright/CopyRight.jsx
@@ -8,7 +8,7 @@
 
 const CopyRight = () =>{
     return(
-        <div className="w-full bg-white py-4">
+        <div className="w-full bg-white py-6">
             <p className=" text-center font-thin text-gray-800">@2024 Nava Jyoti Vidyapith School. Created By <span className="font-normal text-light-textColor">Wish Education</span> </p>
         </div>
     )
