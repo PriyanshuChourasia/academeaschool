594fbf17c31d325f09339249c89fb253428dd032
Priyanshu Chourasia <priyanshu@primesystech.com>
2024-03-29 15:32:31 +0530

    changes done for event

--- src/components/HomeComponents/StudentLife.jsx
+++ src/components/HomeComponents/StudentLife.jsx
@@ -29,7 +29,7 @@ const StudentLife = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div className="grid mobile-xs:mt-2 mobile-sm:mt-2 mobile-md:mt-2 mobile-xs:grid-rows-2 mobile-sm:grid-rows-2 mobile-md:grid-rows-2 desktop-xs:grid-cols-2 desktop-sm:grid-cols-2 desktop-md:grid-cols-2 desktop-lg:grid-cols-2 desktop-xl:grid-cols-2 desktop-2xl:grid-cols-2">
+                <div className="grid mobile-xs:mt-2 mobile-sm:mt-2 mobile-md:mt-2 mobile-xs:grid-rows-2 mobile-sm:grid-rows-2 mobile-md:grid-rows-2 desktop-xs:grid-cols-2 desktop-sm:grid-cols-2 desktop-md:grid-cols-2 desktop-lg:grid-cols-2 desktop-xl:grid-cols-2 desktop-2xl:grid-cols-2">
                     <div className="py-14 px-24 mobile-xs:px-8 mobile-sm:px-10 mobile-md:px-12 bg-light-darkPrimary">
                         <p className="text-3xl text-center text-gray-400 underline hover:text-gray-100 cursor-pointer  mobile-xs:text-2xl mobile-sm:text-3xl mobile-md:text-3xl">Knowledge Hub Sanctuary</p>
                         <p className="text-gray-200 pt-10 text-justify mobile-xs:text-[16px] mobile-sm:text-[17px] mobile-md:text-[19px]">Journey into the realm of wisdom at our school library—where every page holds a universe waiting to be explored. Immerse yourself in knowledge, spark your curiosity, and unlock endless possibilities. From classic tales to modern adventures, our library is a haven for minds eager to learn. </p>
@@ -40,7 +40,7 @@ const StudentLife = () => {
                     <div className={`${HomeCss.applyBackImg}`}>
 
                     </div>
-                </div> */}
+                </div>
             </div>
 
         </div>
