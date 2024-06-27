15c7b4450ebfe2593e22a771c20b83cbc1fd5eaf
PriyanshuChourasia <PriyanshuChourasia@users.noreply.github.com>
2024-06-27 15:21:16 +0000

    deploy: 9b4a530cc16d3fd03df302909222b4c96fd005d6

--- assets/ErrorPage-2_s1rniW.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-YkpanPjB.js";const t=()=>r.jsx(r.Fragment,{children:"this is error page"});export{t as default};
---
+++ assets/ErrorPage-YGXQKCsr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Z5ZnsGRE.js";const t=()=>r.jsx(r.Fragment,{children:"this is error page"});export{t as default};
